8aa6c116d2df0f66192c727484c3161f003ef21d
ZJ1231 <981807576@qq.com>
2018-05-26 17:11:13 +0800

    1.2

---
+++ mvvm-institute/1.2/index.js
@@ -0,0 +1,12 @@
+var MyApp = san.defineComponent({
+    template: '<p>Hello {{name}}!</p>',
+    
+    initData: function () {
+        return {
+            name: 'World'
+        };
+    }
+});
+
+var myApp = new MyApp();
+myApp.attach(document.body);
